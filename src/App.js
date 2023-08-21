31cde16f18a8dbf3b354eb90b9c17071ec00267b
Om-Gor <202101484@daiict.ac.in>
2023-08-21 14:20:20 +0530

    project using react (from YT)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/Navbar';
+import PreNavbar from './component/PreNavbar';
+import Slider from './component/Slider';
+import { BrowserRouter as Router } from 'react-router-dom';
+import data from './data/data.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Router>
+   <PreNavbar/>
+   <Navbar/>
+   {data && data.banner && data.banner.start && data.banner.start.length > 0 ? (
+          <Slider start={data.banner.start} />
+        ) : (
+          <p>No banner images found.</p>
+        )}
+    
+    </Router>
+
+
+   </>
   );
 }
 
